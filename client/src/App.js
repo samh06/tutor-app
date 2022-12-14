f2bdcad91e0db189959533363df68b27358352b2
Will Coggins <110357970+srcoggin@users.noreply.github.com>
2022-12-14 11:51:57 +1000

    done being goofy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, Col, Button, Form } from "react-bootstrap";
 import "./App.css";
 import LoginExternel from "./Login";
+
+
 function App() {
   const [backendData, setBackendData] = useState([{}]);
   const logo = require("./assets/tutor-app-logos_transparent_alt.png");
@@ -23,27 +25,53 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1> Login In!</h1>
-      <spacer type="horizontal" width="5" height="5">
-        {" "}
-        ‎{" "}
-      </spacer>
-      <form onsubmit="console.log('You clicked submit.'); return false">
-        <label>
-          Email:
+
+    <div className="color-overlay d-flex justify-content-center align-items-center" onsubmit="console.log('You clicked submit.'); return false">
+      <Form className="rounded p-4 p-sm-3">
+        <Form.Group className="mb-3"
+        controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
           <input id="name" type="text" name="email" />
-        </label>
-        <spacer type="horizontal" width="5" height="5">
-          {" "}
-          ‎{" "}
-        </spacer>
-        <label>
-          Password:
+          <Form.Text classNmae="text-muted">
+            Enter Email
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3"
+        controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
           <input id="password" type="text" name1="password" />
-        </label>
-      </form>
-      <button onClick={Login}>Log In </button>
+          <Form.Text classNmae="text-muted">
+            Enter Password
+          </Form.Text>
+        </Form.Group>
+        
+        <Form.Group className="mb-3"
+        controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remeber Me" />
+        </Form.Group>
+        
+
+       
+       
+      <style type="text/css">
+        {`
+    .btn-flat {
+      background-color: purple;
+      color: white;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+    }
+    `}
+      </style>
+
+      <Button variant="flat" size="xl" onClick={Login}>
+        Log In
+      </Button>
+
       <spacer type="horizontal" width="5" height="5">
         {" "}
         ‎{" "}
@@ -53,9 +81,12 @@ function App() {
         Not Already Signed Up?, Click Here:{" "}
         <a href="http://example.com">Sign Up!</a>
       </p>
+      </Form>
     </div>
+
   );
 }
+
 
 export default App;
 
